0282da495297fafd693dffaec46bb78288385eff
EllieAzaveda <elliemiller66@icloud.com>
2021-05-12 19:04:04 -0600

    Add movie data import and resolvve errors

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import movieData from 'put your filepath here'
+import { movieData } from './../movieData'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
