48a21aaf89e15156707ab76a0f2ed2caa576dba7
Angela Battillo <angela.battillo@gmail.com>
2021-05-13 17:35:03 -0600

    Reorganize variable structure

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import MovieCard from '../Movie-Card/MovieCard';
+import './Movies.css'
 
 const Movies = ({ movieData }) => {
-  return (
-    <div className='movies'>
-    <h1>It's a thang!</h1>
-    {
-      movieData.map(movie => {
-        return <MovieCard key={movie.id} movie={movie}/>
-      })
-    }
-    </div>
-  )
+  const movieDisplay = movieData.map(movie => {
+      return (
+        <div className='movies-container'>
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+          />
+        </div>
+    )
+  })
+  return movieDisplay
 }
-
 export default Movies;
