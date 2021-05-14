0acf5fd3734a14d05f208b7de27c61fdf58b6d38
Angela Battillo <angela.battillo@gmail.com>
2021-05-13 20:09:27 -0600

    Create basic styling for the page

--- src/Movie-Card/MovieCard.js
+++ src/Movie-Card/MovieCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './MovieCard.css'
 
 const MovieCard = ({ movie }) => {
   return (
-    <div>
-      <img src={movie.poster_path}/>
-      <h2>{movie.title}</h2>
-      <h4>{movie.release_date}</h4>
-      <h4>{movie.average_rating}</h4>
+    <div className='card'>
+      <img className='movie-image' src={movie.poster_path}/>
+      <h2 className='title'>{movie.title}</h2>
+      <h4 className='movie-release'>{movie.release_date}</h4>
+      <h4 className='movie-rating'>{movie.average_rating}</h4>
     </div>
   )
 }
