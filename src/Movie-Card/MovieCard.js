0ebebf3ceb602b1b98e8a15e1988137637622f0f
EllieAzaveda <elliemiller66@icloud.com>
2021-05-15 17:15:19 -0600

    Remove release datte from movie card

--- src/Movie-Card/MovieCard.js
+++ src/Movie-Card/MovieCard.js
@@ -11,7 +11,6 @@ const MovieCard = ({ movie, handleClick }) => {
     <article onClick={() => handleClick(movie.id)}className='card'>
       <img className='movie-image' src={movie.poster_path}/>
       <h2 className='title'>{movie.title}</h2>
-      <h4 className='movie-release'>{movie.release_date}</h4>
       <h4 className='movie-rating'>{taintedRating.repeat(Math.floor(movie.average_rating))}</h4>
     </article>
   )
