dd3f6d1362b8387594782cf98262e8a476ff643c
EllieAzaveda <elliemiller66@icloud.com>
2021-05-15 15:39:09 -0600

    Add finished fetch for a single movie

--- src/utils/APICalls.js
+++ src/utils/APICalls.js
@@ -1,3 +1,5 @@
+export const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2';
+
 export const checkForErr = (response) => {
   if(!response) {
     throw new Error('Something went wrong! Please try again later.')
@@ -5,8 +7,6 @@ export const checkForErr = (response) => {
     return response.json()
   }
 }
-
-export const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2';
 
 export const fetchAllMovies = () => {
   return fetch(`${baseURL}/movies`)
@@ -17,5 +17,9 @@ export const fetchAllMovies = () => {
 }
 
 export const fetchSingleMovie = () => {
-
+  return fetch(`${baseURL}/movies/:movie_id`)
+  .then(checkForErr)
+  .then(singleMovieData => {
+    return singleMovieData
+  })
 }
