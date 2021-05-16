7c9aa506ffee26c74bd2cba3ee24bd337c4e824a
EllieAzaveda <elliemiller66@icloud.com>
2021-05-15 20:14:47 -0600

    Change ShowDetails to class component to hold single data

--- src/Details/ShowDetails.js
+++ src/Details/ShowDetails.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import movieData from './../movieData'
+import React, { Component } from 'react';
 import './ShowDetails.css'
+import { fetchSingleMovie } from '../utils/APICalls'
 
-const ShowDetails = ({ movie, handleClick }) => {
-  return (
-    <div className='card'>
-    <button onClick={() => handleClick(false)}>Home Page</button>
-      <img className='release-date' src={movie.poster_path}/>
-      <h2 className='title'>{movie.title}</h2>
-      <h4 className='movie-release'>{movie.release_date}</h4>
-      <h4 className='movie-rating'>{movie.average_rating}</h4>
-      <h4 className='overview'>{movie.overview}</h4>
-      <h4 className='genre'>{movie.genres}</h4>
-      <h4 className='budget'>{movie.budget}</h4>
-    <h4 className='revenue'>{movie.revenue}</h4>
-      <h4 className='runtime'>{movie.runtime}</h4>
-      <h4 className='tagline'>{movie.tagline}</h4>
-    </div>
-)}
+class ShowDetails extends Component {
+  constructor({ movie }) {
+    super();
+      this.state = {
+        movieID: movie.id,
+        specificMovie: {},
+        movieSelected: movie.id || false,
+        error: ''
+      }
+  }
+
+
+}
 
 export default ShowDetails;
