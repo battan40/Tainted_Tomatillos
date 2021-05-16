c945265a8d53a5c64e2ebbb432f89f4592c7c30e
EllieAzaveda <elliemiller66@icloud.com>
2021-05-15 20:12:44 -0600

    Fix Sprops being passed to ShowDetails

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -4,34 +4,32 @@ import './Movies.css'
 import ShowDetails from '../Details/ShowDetails'
 
 const Movies = ({ movieData, movieSelected, handleClick }) => {
-
   const movieDisplay = () => {
     if(movieSelected) {
       const selection = movieData.find(movie => movie.id === movieSelected)
-        return <ShowDetails
-              movie={selection}
-              handleClick={handleClick}
-             />
-
+        return (
+          <ShowDetails
+            movie={selection}
+            handleClick={handleClick}
+          />
+        )
     } else {
       return movieData.map(movie => {
-      return (
+        return (
           <MovieCard
             key={movie.id}
             movie={movie}
             handleClick={handleClick}
           />
-    )
-  })
-}}
+        )
+      })
+  }}
 
   return (
     <div className='movie-container'>
       {movieDisplay()}
     </div>
   )
-  //the condition needs to render the container of the selected movie, and its specific details
-  //
-  // We need a movies container within a condition
 }
+
 export default Movies;
