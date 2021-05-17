4ac160ec6fd62e8aa15b208c96b3883895a22bd4
EllieAzaveda <elliemiller66@icloud.com>
2021-05-16 18:54:36 -0600

    Fix warning from import

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from '../Movies/Movies'
-import ShowDetails from '../Details/ShowDetails'
 import { fetchAllMovies } from '../utils/APICalls'
 
 class App extends Component {
