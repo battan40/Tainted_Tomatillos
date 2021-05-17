f0ed4637cc292b2a2831030e92bf90161705e168
EllieAzaveda <elliemiller66@icloud.com>
2021-05-16 18:34:27 -0600

    Add fix to main render in show details

--- src/Details/ShowDetails.js
+++ src/Details/ShowDetails.js
@@ -12,24 +12,26 @@ class ShowDetails extends Component {
         error: ''
       }
   }
+
   render() {
     const { handleClick } = this.props;
+    const { specificMovie } = this.state;
     const taintedRating = '⭐️';
-
+    const accessDate = specificMovie.release_date ? specificMovie.release_date.split('-')[0] : '';
     if(this.state.movie && !this.state.error) {
       return (
         <div className='details-card'>
-        <button className='home-button' onClick={() => handleClick(false)}>Back to Home Page</button>
-          <img className='details-image' src={this.state.specificMovie.poster_path}/>
-          <h2 className='details-title'>{this.state.specificMovie.title}</h2>
-          <h4 className='movie-release'>{this.state.specificMovie.release_date}</h4>
-          <h4 className='movie-rating'>{taintedRating.repeat(Math.floor(this.state.specificMovie.average_rating))}</h4>
-          <p className='overview'>{this.state.specificMovie.overview}</p>
-          <h4 className='genre'>Genre: {this.state.specificMovie.genres}</h4>
-          <h4 className='budget'>Budget: {this.state.specificMovie.budget}</h4>
-          <h4 className='revenue'>Revenue: ${this.state.specificMovie.revenue}</h4>
-          <h4 className='runtime'>RunTime: {this.state.specificMovie.runtime} min</h4>
-          <h4 className='tagline'>{this.state.specificMovie.tagline}</h4>
+         <button className='home-button' onClick={() => handleClick(false)}>Home Page</button>
+           <img className='details-image' src={specificMovie.poster_path}/>
+           <h2 className='details-title'>{specificMovie.title}</h2>
+           <h4 className='movie-release'>{accessDate}</h4>
+           <h4 className='movie-rating'>{taintedRating.repeat(Math.floor(specificMovie.average_rating))}</h4>
+           <h4 className='tagline'>{`'${specificMovie.tagline}'`}</h4>
+           <h4 className='overview'>{specificMovie.overview}</h4>
+           <h4 className='genre'>Genre: {specificMovie.genres}</h4>
+           <h4 className='budget'>Budget: {specificMovie.budget}</h4>
+           <h4 className='revenue'>Revenue: ${specificMovie.revenue}</h4>
+           <h4 className='runtime'>RunTime: {specificMovie.runtime} min</h4>
         </div>
       )
     } else if (!this.state.movie && this.state.error) {
