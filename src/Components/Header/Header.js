f1ffaae0e42c2effd467105e8c2d8829eb719bb1
EllieAzaveda <elliemiller66@icloud.com>
2021-05-17 17:38:26 -0600

    Add render method

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
 import './Header.css';
+
+const Header = () => {
+  render() {
+    return (
+      <header className='App'>
+        <h1>Tainted Tomatillos</h1>
+        <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
+      </header>
+    )
+  }
+}
+
+export default Movies;
