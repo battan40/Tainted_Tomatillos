3a8d3ba448aab3f172bced1fcb9cd2691665a4a8
Angela Battillo <angela.battillo@gmail.com>
2021-05-18 10:59:36 -0600

    Add text type

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <h1 >Tainted Tomatillos</h1>
       <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
       <button className='search-button' >Search</button>
-      <input className='search-input' placeholder='Search for a Movie'/>
+      <input className='search-input' type='text' placeholder='Search for a Movie'/>
     </header>
   )
 }
