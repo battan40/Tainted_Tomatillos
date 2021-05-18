ac5569cbac56961574cf13a6f107e9bb2b1ccff2
EllieAzaveda <elliemiller66@icloud.com>
2021-05-17 18:59:10 -0600

    Fix return statement

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import './Header.css';
 
 const Header = () => {
-  render() {
-    return (
-      <header className='App'>
-        <h1>Tainted Tomatillos</h1>
-        <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
-        <button>Login</button>
-        <input>Search for a Movie</input>
-      </header>
-    )
-  }
+  return (
+    <header className='App'>
+      <h1>Tainted Tomatillos</h1>
+      <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
+      <button>Login</button>
+      <input>Search for a Movie</input>
+    </header>
+  )
 }
 
 export default Movies;
