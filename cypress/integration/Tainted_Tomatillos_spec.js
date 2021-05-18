644f2b404bdc63e38054b560d32ec697a86109ef
Angela Battillo <angela.battillo@gmail.com>
2021-05-17 22:55:17 -0600

    Implement functionality of intercept of get call with mock data  object

--- cypress/integration/Tainted_Tomatillos_spec.js
+++ cypress/integration/Tainted_Tomatillos_spec.js
@@ -2,7 +2,13 @@ describe('Show main view of Tainted Tomatillos App', () => {
 
   beforeEach(() => {
     cy.fixture('stubbingMockData.json')
-    
+      .then(mockData =>{
+        cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+          statusCode: 201,
+          delay: 100,
+          body: mockData
+        })
+      })
     cy.visit('http://localhost:3000')
   })
 
