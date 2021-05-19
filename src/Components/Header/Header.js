83fdbad612887dd5f295727edbd568ccbbdbdd2d
EllieAzaveda <elliemiller66@icloud.com>
2021-05-18 19:26:09 -0600

    Wrap header elements in Router Link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 const Header = () => {
   return (
-    <header className='App'>
+    <Link to={'/'}><header className='App'>
       <h1>Tainted Tomatillos</h1>
       <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
       <button>Login</button>
       <input>Search for a Movie</input>
-    </header>
+    </header></Link>
   )
 }
 
