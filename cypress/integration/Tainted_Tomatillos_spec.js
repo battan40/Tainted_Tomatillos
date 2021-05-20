db3b0a37318c3eb409b73ae93639a7cb57111fc7
Angela Battillo <angela.battillo@gmail.com>
2021-05-19 20:01:11 -0600

    Build functionality of header

--- cypress/integration/Tainted_Tomatillos_spec.js
+++ cypress/integration/Tainted_Tomatillos_spec.js
@@ -2,7 +2,7 @@ describe('Show main view of Tainted Tomatillos App', () => {
 
   beforeEach(() => {
     cy.fixture('stubbingMockData.json')
-      .then(mockData =>{
+      .then(mockData => {
         cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
           statusCode: 201,
           delay: 100,
@@ -10,10 +10,15 @@ describe('Show main view of Tainted Tomatillos App', () => {
         })
       })
     cy.visit('http://localhost:3000')
-  })
+  });
 
-  it('should be able to visit the page', () => {
+  it('Should be able to visit the main page', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   });
 
-})
+  it('Should display the title, subtitle, button, and input of the page upon loading', () => {
+      cy.contains('header>h1', 'Tainted Tomatillos')
+        .get('header .small-title').should('contain', 'Where')
+        .get('header').find('button').should('be.visible')
+        .get('header input[type=text]').should('be.visible')
+  });
