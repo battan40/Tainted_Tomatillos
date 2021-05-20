5a4b940d1879ff767c39dc34b3da082a8971ff34
EllieAzaveda <elliemiller66@icloud.com>
2021-05-19 19:24:59 -0600

    Change render to a const

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
-import { baseURL, checkForErr, fetchAllMovies, fetchSingleMovie } from './APICalls';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const router = <BrowserRouter> <App /> </BrowserRouter>;
+const router = (<BrowserRouter> <App /> </BrowserRouter>);
 
 ReactDOM.render(router, document.getElementById('root'));
 
