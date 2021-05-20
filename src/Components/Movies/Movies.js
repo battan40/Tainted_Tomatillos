4fb640b387200d912372e0fc4360e108c6d4db75
Angela Battillo <angela.battillo@gmail.com>
2021-05-20 17:51:46 -0600

    Experiment with nav link styling

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -18,7 +18,7 @@ const Movies = ({ movieData, movieSelected, handleClick }) => {
   }
 
   return (
-    <NavLink to={'/'}><div className='movie-container'>
+    <NavLink to={'/'}><div className='movie-container title movie-rating' >
       {movieDisplay()}
     </div></NavLink>
   )
