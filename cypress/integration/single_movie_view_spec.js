01d7d1f433f5ba0de3213802c2bd2c6211e534aa
EllieAzaveda <elliemiller66@icloud.com>
2021-05-20 21:23:35 -0600

    Add test to check that only one movie rdetails card enders to page

--- cypress/integration/single_movie_view_spec.js
+++ cypress/integration/single_movie_view_spec.js
@@ -16,4 +16,16 @@ describe('Show single movie view of Tainted Tomatillos App', () => {
     cy.url().should('eq', 'http://localhost:3000/movieDetails/337401')
   });
 
+  it('Should display the title, subtitle, button, and input of the page upon loading', () => {
+    cy.contains('nav>h1', 'Tainted Tomatillos')
+      .get('nav .small-title').should('contain', 'Where The Ratings Are Honestly Rotten')
+      .get('nav').find('button').should('be.visible')
+      .get('nav input[type=text]').should('be.visible')
+  });
+
+  it('Should only display one movies on the details page', () => {
+    cy.get('article').find('.details-card').should('have.length', 1)
+  });
+
+
 })
