a9783e7b340fb77f7146db45af9a321f538ba70a
EllieAzaveda <elliemiller66@icloud.com>
2021-05-21 16:11:41 -0600

    Add backdrop image

--- src/Components/Details/ShowDetails.js
+++ src/Components/Details/ShowDetails.js
@@ -22,25 +22,27 @@ class ShowDetails extends Component {
     const taglineConditional = specificMovie.tagline ? specificMovie.tagline : 'Write in and give us a tagline for this one!';
 
         return (
-          <div className='details-card'>
-            <NavLink exact to='/'><button data-cy='home-button' className='home-button' onClick={() => handleClick(false)}>Back to Home Page</button></NavLink>
-            {this.state.error && <h3>{this.state.error}</h3>}
-            {!this.state.error &&
-              <>
-                <img className='details-image' src={specificMovie.poster_path} alt={`movie poster for ${specificMovie.title}`}/>
-                <h2 data-cy='details-title' className='details-title'>{specificMovie.title}</h2>
-                <h4 data-cy='movie-release' className='movie-release'>{accessDate}</h4>
-                <h4 data-cy='movie-rating' className='movie-rating'>{taintedRating.repeat(Math.floor(specificMovie.average_rating))}</h4>
-                <h4 data-cy='tagline' className='tagline'>{`'${taglineConditional}'`}</h4>
-                <h4 data-cy='overview-label' className='overview'>Movie Overview:</h4>
-                <h4 data-cy='overview' className='overview'>{specificMovie.overview}</h4>
-                <h4 data-cy='genre' className='genre'>Genre: {specificMovie.genres}</h4>
-                <h4 data-cy='budget' className='budget'>Budget: ${specificMovie.budget}</h4>
-                <h4 data-cy='revenue' className='revenue'>Revenue: ${specificMovie.revenue}</h4>
-                <h4 data-cy='runtime' className='runtime'>RunTime: {specificMovie.runtime} min</h4>
-              </>
-            }
-          </div>
+          <article className='details-display' style={{backgroundImage: `url(${specificMovie.backdrop_path})`}}>
+            <div className='details-card'>
+              <NavLink exact to='/'><button data-cy='home-button' className='home-button' onClick={() => handleClick(false)}>Back to Home Page</button></NavLink>
+              {this.state.error && <h3>{this.state.error}</h3>}
+              {!this.state.error &&
+                <>
+                  <img className='details-image' src={specificMovie.poster_path} alt={`movie poster for ${specificMovie.title}`}/>
+                  <h2 data-cy='details-title' className='details-title'>{specificMovie.title}</h2>
+                  <h4 data-cy='movie-release' className='movie-release'>{accessDate}</h4>
+                  <h4 data-cy='movie-rating' className='movie-rating'>{taintedRating.repeat(Math.floor(specificMovie.average_rating))}</h4>
+                  <h4 data-cy='tagline' className='tagline'>{`'${taglineConditional}'`}</h4>
+                  <h4 data-cy='overview-label' className='overview'>Movie Overview:</h4>
+                  <h4 data-cy='overview' className='overview'>{specificMovie.overview}</h4>
+                  <h4 data-cy='genre' className='genre'>Genre: {specificMovie.genres}</h4>
+                  <h4 data-cy='budget' className='budget'>Budget: ${specificMovie.budget}</h4>
+                  <h4 data-cy='revenue' className='revenue'>Revenue: ${specificMovie.revenue}</h4>
+                  <h4 data-cy='runtime' className='runtime'>RunTime: {specificMovie.runtime} min</h4>
+                </>
+              }
+            </div>
+          </article>
         )
   }
 
