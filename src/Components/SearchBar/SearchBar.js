cd573e38bd2a486807ec4f280be04f98c5055c0a
EllieAzaveda <elliemiller66@icloud.com>
2021-05-22 01:05:52 -0600

    Remove functions to try in App

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import MovieCard from '../Movie-Card/MovieCard';
+import { Route, NavLink } from 'react-router-dom';
 import './SearchBar.css';
 
 class SearchBar extends Component {
-  constructor() {
+  constructor({ movieData, handleClick }) {
     super();
       this.state = {
+        movies: movieData,
+        matchingMovies: null,
         searchInput: '',
         error: ''
       }
   }
 
   handleChange = (event) => {
-    let searchValue = event.target.value;
+    event.preventDefault();
+    let searchValue = (event.target.value).toUpperCase();
     this.setState({ searchInput: searchValue })
   }
+
 
   render () {
     return (
@@ -22,14 +27,14 @@ class SearchBar extends Component {
         <input
           className='search-input'
           type='text'
-          placeholder="Search for a Movie"
+          placeholder="Search for a Movie or Genre"
           value={this.state.searchInput}
           onChange={(event) => this.handleChange(event)}
         />
-        <button className='search-button'>Search</button>
       </form>
     )
   }
 }
+
 
 export default SearchBar;
