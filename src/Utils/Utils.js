b77a3d151197f621d69c7112cccce6746e0df36c
Angela Battillo <angela.battillo@gmail.com>
2021-05-21 23:18:32 -0600

    Clean data function creation

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -2,3 +2,14 @@ export const taintedAverage = (movieRating) => {
   const taintedRating = '⭐️'
   return taintedRating.repeat(Math.floor(movieRating))
 }
+
+export const cleanAllMoviesData = (allMovies) => {
+  return allMovies.map(movie => {
+    return {
+      "id": movie.id,
+      "poster_path": movie.poster_path,
+      "title": movie.title,
+      "average_rating": movie.average_rating
+    }
+  })
+}
