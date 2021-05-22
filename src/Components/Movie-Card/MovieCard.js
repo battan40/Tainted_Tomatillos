15e4a53a951315b1a762c743c4f1bcf6a140477c
Angela Battillo <angela.battillo@gmail.com>
2021-05-21 23:08:54 -0600

    Move functionality to dry code via utils file

--- src/Components/Movie-Card/MovieCard.js
+++ src/Components/Movie-Card/MovieCard.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { taintedAverage } from '../../Utils/Utils';
 import './MovieCard.css';
 
 const MovieCard = ({ movie }) => {
-  const taintedRating = '⭐️'
-  const taintedAverage = taintedRating.repeat(Math.floor(movie.average_rating))
 
   return (
     <NavLink id={movie.id} to={`/movieDetails/${movie.id}`} >
       <div className='card'>
         <img className='movie-image' src={movie.poster_path} alt={`${movie.title} poster`}/>
         <h2 className='title'>{movie.title}</h2>
-        <h4 className='movie-rating'>{taintedAverage}</h4>
+        <h4 className='movie-rating'>{taintedAverage(movie.average_rating)}</h4>
       </div>
     </NavLink>
 
