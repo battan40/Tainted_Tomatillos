70284f92979e0b640a5bd2f9f6ae7bb59b6b1b6d
EllieAzaveda <elliemiller66@icloud.com>
2021-05-22 01:06:47 -0600

    Add search methods filter and handleChange

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
+// import SearchBar from '../SearchBar/SearchBar';
 import Movies from '../Movies/Movies';
 import ShowDetails from '../Details/ShowDetails';
-import SearchBar from '../SearchBar/SearchBar';
 import { Route, Switch } from 'react-router-dom';
 import { fetchAllMovies } from '../../APICalls';
 import './App.css';
@@ -13,6 +13,9 @@ class App extends Component {
       this.state = {
         movies: [],
         movieSelected: false,
+        matchingMovies: null,
+        isRendered: false,
+        searchInput: '',
         error: ''
     }
   }
@@ -34,15 +37,34 @@ class App extends Component {
       <>
       <article className='App'>
         <Header />
-        <SearchBar />
+        <form>
+          <input
+            className='search-input'
+            type='text'
+            placeholder="Search for a Movie"
+            value={this.state.searchInput}
+            onChange={(event) => this.handleChange(event)}
+          />
+        </form>
         {this.state.error && <h3>{this.state.error}</h3>}
-        {!this.state.error &&
+        {!this.state.error && this.state.matchingMovies &&
+            <Route exact path="/" render={() => {
+                return <Movies
+                  movieData={this.state.matchingMovies}
+                  movieSelected={this.state.movieSelected}
+                  handleClick={this.handleClick}
+                  matchingMovies={this.matchingMovies}/>
+              }}
+            />
+        }
+        {!this.state.error && !this.state.matchingMovies &&
           <Switch>
             <Route exact path="/" render={() => {
                 return <Movies
                   movieData={this.state.movies}
                   movieSelected={this.state.movieSelected}
-                  handleClick={this.handleClick}/>
+                  handleClick={this.handleClick}
+                  matchingMovies={this.matchingMovies}/>
               }}
             />
             <Route path="/movieDetails/:id" render={({ match }) => {
@@ -71,6 +93,23 @@ class App extends Component {
       })
       .catch(err => err.message)
   }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    let searchValue = (event.target.value).toUpperCase();
+    let matches = this.filterMovies(searchValue);
+    this.setState({ matchingMovies: matches });
+    console.table(matches)
+
+  }
+
+  filterMovies = (searchValue) => {
+    return this.state.movies.filter(movie => {
+      let capTitle = movie.title.toUpperCase();
+      return capTitle.includes(searchValue)
+    })
+  }
+
 }
 
 export default App;
