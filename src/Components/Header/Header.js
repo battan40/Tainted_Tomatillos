bb2478b70a739548dc48359e3aca123481b566d0
Angela Battillo <angela.battillo@gmail.com>
2021-05-23 13:29:11 -0600

    Give page title a class name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = ({ moviesForSearchBar }) => {
   return (
     <nav className='header'>
-      <h1>Tainted Tomatillos</h1>
+      <h1 className='large-title'>Tainted Tomatillos</h1>
       <h2 className='small-title'>Where The Ratings Are Honestly Rotten</h2>
       <SearchBar moviesForSearchBar={moviesForSearchBar}/>
     </nav>
