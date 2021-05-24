46cbe006936089add0ed20ecdd6276bbe7f6afe6
EllieAzaveda <elliemiller66@icloud.com>
2021-05-24 16:38:42 -0600

    Pass in searched props

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -2,21 +2,26 @@ import React from 'react';
 import MovieCard from '../Movie-Card/MovieCard';
 import './Movies.css';
 
-const Movies = ({ movieData }) => {
+const Movies = ({ movieData, searched }) => {
   const movieDisplay = () => {
-      return movieData.map(movie => {
-        return (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-          />
-        )
-      })
+    return movieData.map(movie => {
+      return (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+        />
+      )
+    })
   }
 
   return (
     <div className='movie-container'>
-      {movieDisplay()}
+    {!searched &&
+      {this.movieDisplay()}
+    }
+    {searched && movieData.length &&
+      <h3>{'Sorry! No movies were found!'}</h3>
+    }
     </div>
   )
 }
